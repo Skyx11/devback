2e87aa36dfe46d7707a17da4f16616f8354b7ac0
unknown <t.dedain@student.isartdigital.com>
2018-10-12 14:42:33 +0200

    update

---
+++ calculatrice.js
@@ -0,0 +1,28 @@
+console.log('coucou');
+
+
+/* déclaration de variables :
+c = 0
+a = 2
+b = 3
+//addition
+Début : 
+    c = a+b
+    afficher a+" + "+b+" = "+c
+Fin */
+
+
+var c = 0, a=2, b=3;
+
+
+c = a + b;
+console.log(a+ '+' +b+ '=' +c);
+
+c = a - b;
+console.log(a+  '-' +b+ '=' +c);
+
+c = a / b;
+console.log(a+ '/' +b+ '=' +c);
+
+c = a * b;
+console.log(a+ 'x' +b+ '=' +c);
